6d53d47660c428ed2889e39aece859e99489229e
Andromeda <stroev.andrew@gmail.com>
2025-10-10 01:34:59 +0100

    Implement line drawing

--- main.go
+++ main.go
@@ -8,10 +8,30 @@ func main() {
 	rl.SetWindowState(rl.FlagWindowUndecorated)
 	defer rl.CloseWindow()
 
+	points := []rl.Vector2{}
+	isDrawing := false
+
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.Blank)
-		rl.DrawText("Hello, World!", 10, 10, 20, rl.Red)
+
+		isMouseDown := rl.IsMouseButtonDown(rl.MouseLeftButton)
+		if isMouseDown {
+			if !isDrawing { // on mouse down
+				println("Start drawing")
+				isDrawing = true
+			}
+			points = append(points, rl.GetMousePosition())
+		} else if isDrawing { // on mouse up
+			println("Stop drawing")
+			isDrawing = false
+			points = []rl.Vector2{}
+		}
+
+		for i := 1; i < len(points)-1; i++ {
+			rl.DrawLineEx(points[i], points[i+1], 5, rl.Magenta)
+		}
+
 		rl.EndDrawing()
 	}
 }
